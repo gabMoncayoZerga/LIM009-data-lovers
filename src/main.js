5be31d04d391e6b821ff535f63b9a33b197981a6
gabriela2393 <gabymoncayozerga23@gmail.com>
2019-03-17 23:10:58 -0500

    test objeto Injuries

--- src/main.js
+++ src/main.js
@@ -2,34 +2,11 @@
 /*JavaScript: Cómo filtrar un array de objetos basado en un atributo
 El elemento filter arroja un array dada una condición
 */
+//CREANDO COPIA DE LA DATA//
 
-const copiadeData=INJURIES.slice();
-
-const arrNullBoating=copiadeData.filter(function mostrandoIndicador(elemento) {
-  return(elemento.Total_Injured_Persons_Recreational_Boating==="null")
-});
-
-
-console.log(arrNullBoating);
-
-/*
-const arr =[
-  {
-    "home-id":"1",
-    "price":"925",
-    "sqtf":"1100",
-    "num_of_beds":"2",
-    "num_of_baths": "2.0",
-  },
-  {
-    "home-id":"2",
-    "price":"1425",
-    "sqtf":"1900",
-    "num_of_beds":"4",
-    "num_of_baths":"2.5"
+  const copyData = (wbank) => {
+  const wbankData = [];
+  for (let i = 0; i < wbank.length; i++) {
+    wbankData.push(Object.assign({}, wbank[i]));
   }
-];
-const arrPrice = arr.filter(function (elemento) {
-return(elemento.price==="1425");
-});
-console.log(arrPrice);*/
+  return wbankData;
