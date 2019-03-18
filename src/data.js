d16083c42e695fc0ac38d6a020d8b14b879cf0c9
gabriela2393 <gabymoncayozerga23@gmail.com>
2019-03-18 07:06:39 -0500

    intento sort

--- src/data.js
+++ src/data.js
@@ -3,8 +3,24 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
 
-window.example = example;
+
+
+
+
+
+/*
+const sortData=(data, firstYear, lastYear)=>{
+
+  const getYearsOfArray=data.slice(firstYear(1960), lastYear(2016));
+
+if (firstYear>lastYear) {
+    return 1;
+  }
+  if (firstYear>lastYear) {
+    return -1
+  }
+  return 0;
+}
+
+console.log(sortData);*/
--- src/main.js
+++ src/main.js
@@ -2,11 +2,3 @@
 /*JavaScript: Cómo filtrar un array de objetos basado en un atributo
 El elemento filter arroja un array dada una condición
 */
-//CREANDO COPIA DE LA DATA//
-
-  const copyData = (wbank) => {
-  const wbankData = [];
-  for (let i = 0; i < wbank.length; i++) {
-    wbankData.push(Object.assign({}, wbank[i]));
-  }
-  return wbankData;
