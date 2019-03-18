1561f8675e0a7ba294084324a4a2aa350c8f834f
NardaC <nardac6@gmail.com>
2019-03-17 21:17:20 -0500

    filtrando en consola y html mejorado

--- src/data.js
+++ src/data.js
@@ -3,8 +3,32 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = () => {
+//   return 'example';
+// };
 
-window.example = example;
+// window.example = example; 
+
+const data = INJURIES;
+
+
+function FilterYear(){//filter
+  
+  let selectYear = document.getElementById("selected_year").value;
+    // return year==selectYear;//
+    const dataYear = data.filter(dat => dat.Year === selectYear);//year
+
+console.log (dataYear);
+  }
+
+const btnSearch = document.getElementById("search");
+//const btnMaritime =document.getElementById("Btn_maritime");
+//const btnLand =document.getElementById("Btn_land");
+
+btnSearch.addEventListener("click",FilterYear);
+//creo una funcion que mostrará la seccion aereo
+
+
+
+
+
