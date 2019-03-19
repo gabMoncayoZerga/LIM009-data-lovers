4238ee680ea2bad659a9d3f7fcb8a2320d8a7d97
gabriela2393 <gabymoncayozerga23@gmail.com>
2019-03-18 23:35:36 -0500

    flujo de pantallas main.js

--- src/data.js
+++ src/data.js
@@ -1,25 +1,91 @@
-/* Manejo de data */
+const data = INJURIES;
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+
+function FilterYear(){//filter
+
+  let selectYear = document.getElementById("selected_year").value;
+    // return year==selectYear;//
+    const dataYear = data.filter(dat => dat.Year === selectYear);//year
+    console.log(dataYear);
+  }
+
+const btnSearch = document.getElementById("search");
+//const btnMaritime =document.getElementById("Btn_maritime");
+//const btnLand =document.getElementById("Btn_land");
+
+btnSearch.addEventListener("click",FilterYear);
+
 /*
+const dinosaurs =[];
 
+dinosaurs[0]="Tiranosaurio Rex";
+dinosaurs[1]="Velociraptor";
+dinosaurs[2]="Triceratops";
+dinosaurs[3]="Estegosaurio";
+dinosaurs[4]="Troodon";
+dinosaurs[5]="Estiracousario"
 
+dinosaurs[0]="T-Rex";
+
+//Añadiendo elemento en el indice 33
+
+dinosaurs[33]="Philosoraptor";
+console.log(dinosaurs);
+
+const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+const nombresMasculinos=nombres.slice(1,3);
+console.log(nombresMasculinos);
 */
-const data=INJURIES;
+/*let arr=[1,2,15];
+const sortArr=arr.sort();
+console.log(sortArr);
+
+const compareNumeric=(a,b)=>{
+  if (a>b) return  1
+  if (a===b) return 0
+return -1;
+};
+
+arr =[1,2,15];
+console.log(arr.sort(compareNumeric));*/
+
+/*var objs = [
+    { first_nom: 'Lazslo', last_nom: 'Jamf'     },
+    { first_nom: 'Pig',    last_nom: 'Bodine'   },
+    { first_nom: 'Pirate', last_nom: 'Prentice' }
+];
+
+function compare(a,b) {
+  if (a.last_nom < b.last_nom)
+    return -1;
+  if (a.last_nom > b.last_nom)
+    return 1;
+  return 0;
+}
+
+console.log(objs.sort(compare));*/
+
+/*const getData = INJURIES;
+const accedientoElemento=data[2];
+console.log(accedientoElemento);
+/*
+const value = elemento[condition];
+const year = accedientoElemento.Year;
+console.log(year);
+const data = { year.value, value.value };*/
 
 const filterData = (data, condition) => {
-
   // aqui se guardará un objeto
   let resultado = [];
 
-  data.forEach(elem => {
+  data.forEach(elemento => {
 
     //se obtiene el valor del arreglo dada la condicion
-    const value = elem[condition];
-    const year = elem.Year;
+  const value = elemento[condition];
+  const year = elemento.Year;
 
    const data = { year: year, value: value };
+
     //se añade al objeto resultado
     resultado.push(data);
 
@@ -28,8 +94,8 @@ const filterData = (data, condition) => {
   return resultado;
 }
 
-console.log(data,"Total_Injured_Persons_Recreational_Boating");
-
+console.log(filterData(data,"Total_Injured_Persons_Railroad"));
+console.log(filterData(data,"Total_Injured_Persons_General_Aviation"));
 
 
 
--- src/main.js
+++ src/main.js
@@ -2,3 +2,37 @@
 /*JavaScript: Cómo filtrar un array de objetos basado en un atributo
 El elemento filter arroja un array dada una condición
 */
+ /*Capturando elementos de HTML*/
+ const logIn=document.getElementById("login");
+ const submitButton=document.getElementById("submit");
+ const homePage=document.getElementById("home");
+ const filterCategory=document.getElementById("category");
+ const sortData=document.getElementById("select-order");
+ const yearSection=document.getElementById("year-section");
+ const airInjuries=document.getElementById("air-injuries");
+ const maritimeInjuries=document.getElementById("maritime-injuries")
+ const landInjuries=document.getElementById("land-injuries");
+ const news=document.getElementById("news");
+
+logIn.style.display="block";
+submitButton.style.display="block"
+homePage.style.display="none";
+filterCategory.style.display="none";
+sortData.style.display="none";
+yearSection.style.display="none";
+airInjuries.style.display="none";
+maritimeInjuries.style.display="none";
+landInjuries.style.display="none";
+news.style.display="none";
+
+ submitButton.addEventListener("click",()=>{
+   logIn.style.display="none";
+   submitButton.style.display="none";
+   homePage.style.display="block";
+   filterCategory.style.display="block";
+   sortData.style.display="block";
+   yearSection.style.display="block";
+   airInjuries.style.display="block";
+   maritimeInjuries.style.display="block";
+   landInjuries.style.display="block";
+ });
