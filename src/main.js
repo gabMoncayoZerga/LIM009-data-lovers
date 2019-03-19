095b85ad439f69af3df38601cba8bd6b8ab0429c
NardaC <nardac6@gmail.com>
2019-03-19 00:16:03 -0500

    ocultando la seccion login y mostrando home, jalando el nombre que introduce el usuario a la seccion home

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
 /* Manejo del DOM */
+const btnEnter = document.getElementById("btn_enter");
+// const train = document.getElementById("btn_train");
+// const pedalcyclists = document.getElementById("btn_pedacyclists");
+// const motorcyclist = document.getElementById("btn_motorcyclists");
+// const car = document.getElementById("btn_car");
+// const bus = document.getElementById("btn_bus");
+
+const sectionLogin = document.getElementById("login");
+const sectionHome = document.getElementById("home");
+
+sectionLogin.classList.toggle("classShow");
+// sectionHome.classList.toggle("classShow");
+
+btnEnter.addEventListener("click",() => {
+    const user = document.getElementById("user").value;
+    if (user == ""){
+        alert("please, enter your name");
+      } else {
+          document.getElementById("name_user").innerHTML = user;
+          sectionLogin.classList.toggle("classShow");
+          sectionHome.classList.toggle("classShow");
+         }
+});
