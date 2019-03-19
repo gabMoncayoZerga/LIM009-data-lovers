84d08ccaa53d66c429ccbc1afa0b0e27648ed073
gabriela2393 <gabymoncayozerga23@gmail.com>
2019-03-18 22:58:48 -0500

    filtrado por categoría

--- src/data.js
+++ src/data.js
@@ -2,8 +2,33 @@
 
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
+/*
 
 
+*/
+const data=INJURIES;
+
+const filterData = (data, condition) => {
+
+  // aqui se guardará un objeto
+  let resultado = [];
+
+  data.forEach(elem => {
+
+    //se obtiene el valor del arreglo dada la condicion
+    const value = elem[condition];
+    const year = elem.Year;
+
+   const data = { year: year, value: value };
+    //se añade al objeto resultado
+    resultado.push(data);
+
+  });
+
+  return resultado;
+}
+
+console.log(data,"Total_Injured_Persons_Recreational_Boating");
 
 
 
